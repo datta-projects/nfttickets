28e21977c56349d4bc030fbfebed7305dc14ae8e
dattaraj24 <dattarajguttedar1@gmail.com>
2023-11-12 05:32:44 +0000

    added nft contract and marketplace functions

---
+++ contracts/marketplace.sol
@@ -0,0 +1,141 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/common/ERC2981.sol";
+
+contract Marketplace is Ownable {
+
+ uint256 public orderCount;
+ bytes4 private constant _INTERFACE_ID_ERC2981 = 0x2a55205a;
+
+ struct Order {
+  address tokenaddress;
+  uint256 tokenid;
+  address seller;
+  bool issold;
+  uint256 priceinwei;
+ }
+
+ struct offers{
+  uint256 offerprice;
+  address offermadeby;
+  bool offeraccepted;
+ }
+
+ mapping(uint256 => Order)orders;
+ mapping(address =>mapping(uint256 => offers[]))publicoffers;
+
+ event listedforsale(address tokenaddress,uint256 tokenid,uint256 priceinwei,address seller);
+ event listingsold(address seller,address buyer,address tokenaddress,uint256 tokenid,uint256 price);
+ event offermade(address offermadeby, address tokenaddress, uint256 tokenid, uint256 offerpriceinwei);
+ event publicofferaccepted(address offerer, address tokenaddress, uint256 tokenid, uint256 priceaccepted);
+ event RoyaltiesPaid(uint256 tokenId, uint value);
+
+
+ function listforsale(address _tokenaddress, uint256 _tokenid,uint256 _priceinwei)public payable returns(bool){
+  orders[orderCount] = Order(
+    _tokenaddress,
+    _tokenid,
+    msg.sender,
+    false,
+    _priceinwei
+  );
+  ERC721(_tokenaddress).safeTransferFrom(msg.sender,address(this),_tokenid);
+  emit listedforsale(_tokenaddress, _tokenid, _priceinwei, msg.sender);
+  return true;
+ }
+
+ function buyfromlisting(uint256 _listingid)public payable {
+  Order storage order = orders[_listingid];
+  require(msg.value >= order.priceinwei,"value sent should be more than or Equal to listing price");
+  order.issold = true;
+
+  uint256 salevalue = msg.value;
+  if(_checkRoyalties(order.tokenaddress)){
+    salevalue = _deduceRoyalties(order.tokenid,order.priceinwei,order.tokenaddress);
+  }
+  order.seller.call{value: salevalue}('');
+
+  ERC721(order.tokenaddress).safeTransferFrom(address(this),msg.sender, order.tokenid);
+  emit listingsold(order.seller, msg.sender, order.tokenaddress, order.tokenid, msg.value);
+ }
+
+ function makeoffer(address tokenaddress, uint256 tokenid, uint256 offerpriceinwei)public payable {
+  require(msg.value >= offerpriceinwei,"amount not right");
+  publicoffers[tokenaddress][tokenid].push(offers(
+   msg.value,
+   msg.sender,
+   false
+  )
+  );
+  emit offermade(msg.sender, tokenaddress, tokenid, msg.value);
+ }
+
+ function acceptpublicoffer(uint256 offerid,address tokenaddress,uint256 tokenid)public payable{
+  require(ERC721(tokenaddress).ownerOf(tokenid) == msg.sender,"be the owner of nft to accept offers");
+  offers storage offer = publicoffers[tokenaddress][tokenid][offerid];
+  require(!offer.offeraccepted,"cant accept aaccepted offer");
+  uint256 salevalue = offer.offerprice;
+  offer.offeraccepted = true;
+  if(_checkRoyalties(tokenaddress)){
+    salevalue = _deduceRoyalties(tokenid,offer.offerprice,tokenaddress);
+  }
+  msg.sender.call{value: salevalue}('');
+  ERC721(tokenaddress).safeTransferFrom(msg.sender, offer.offermadeby, tokenid);
+  emit publicofferaccepted(offer.offermadeby, tokenaddress, tokenid, offer.offerprice);
+ }
+
+ function _checkRoyalties(address _contract) internal returns (bool) {
+  (bool success) = IERC2981(_contract).
+  supportsInterface(_INTERFACE_ID_ERC2981);
+  return success;
+ }
+
+ function _deduceRoyalties(uint256 tokenId, uint256 grossSaleValue,address _tokenaddres)
+ internal returns (uint256 netSaleAmount) {
+     // Get amount of royalties to pays and recipient
+     (address royaltiesReceiver, uint256 royaltiesAmount) = ERC2981(_tokenaddres)
+     .royaltyInfo(tokenId, grossSaleValue);
+     // Deduce royalties from sale value
+     uint256 netSaleValue = grossSaleValue - royaltiesAmount;
+     // Transfer royalties to rightholder if not zero
+     if (royaltiesAmount > 0) {
+         royaltiesReceiver.call{value: royaltiesAmount}('');
+     }
+     // Broadcast royalties payment
+     emit RoyaltiesPaid(tokenId, royaltiesAmount);
+     return netSaleValue;
+ }
+
+// Add this function to your existing Marketplace contract
+function getPublicOffersByAddress(address offerer) external view returns (offers[] memory) {
+  uint256 numOrders = orderCount;
+  uint256 totalUserOffers = 0;
+  // Count the total number of offers made by the address
+  for (uint256 i = 0; i < numOrders; i++) {
+      totalUserOffers += publicoffers[orders[i].tokenaddress][orders[i].tokenid].length;
+  }
+ // Create a fixed-size array to store the user's offers
+  offers[] memory userOffers = new offers[](totalUserOffers);
+  uint256 currentIndex = 0;
+  // Populate the array with the user's offers
+  for (uint256 i = 0; i < numOrders; i++) {
+      offers[] storage offersArray = publicoffers[orders[i].tokenaddress][orders[i].tokenid];
+
+      for (uint256 j = 0; j < offersArray.length; j++) {
+          if (offersArray[j].offermadeby == offerer) {
+              userOffers[currentIndex] = offersArray[j];
+              currentIndex++;
+          }
+      }
+  }
+
+  return userOffers;
+}
+
+
+
+
+}
---
+++ contracts/nft.sol
@@ -0,0 +1,107 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/token/common/ERC2981.sol";
+
+contract NFTticket is ERC721, Ownable,ERC2981 {
+    uint256 private _nextTokenId;
+
+    string public uriPrefix = "";
+    string public uriSuffix = ".json";
+
+    address private _royaltiesReceiver;
+    // Percentage of each sale to pay as royalties
+    uint256 public constant royaltiesPercentage = 5;
+    // EIP-712 domain separator
+    bytes32 private DOMAIN_SEPARATOR;
+    
+    constructor(address initialOwner,string memory name, string memory symbol,address initialRoyaltiesReceiver, string memory _uri)
+        ERC721(name, symbol)
+    {
+            DOMAIN_SEPARATOR = keccak256(abi.encode(
+            keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+            keccak256(bytes("EventTicket")),
+            keccak256(bytes("1")),
+            block.chainid,
+            address(this)
+        ));
+        _royaltiesReceiver = initialRoyaltiesReceiver;
+        uriPrefix = _uri;
+    }
+
+    function safeMint(address to) public onlyOwner {
+       uint256 tokenId = _nextTokenId++;
+       _safeMint(to, tokenId);
+    }
+
+     function tokenURI(uint256 _tokenId)
+    public
+    view
+    virtual
+    override
+    returns (string memory)
+    {
+   
+    string memory currentBaseURI = _baseURI();
+    return bytes(currentBaseURI).length > 0
+        ? string(abi.encodePacked(currentBaseURI, tostring(uint256(_tokenId)), uriSuffix))
+        : "";
+    }
+
+  function _baseURI() internal view virtual override returns (string memory) {
+    return uriPrefix;
+  }
+
+  function tostring(uint256 _value)public pure returns(string memory){
+   return Strings.toString(_value);
+  }
+
+  
+    // Function to transfer a ticket using an EIP-712 signature
+    function transferWithSignature(address from, address to, uint256 tokenId, bytes memory signature) external {
+        bytes32 hash = keccak256(abi.encodePacked(
+            "\x19\x01",
+            DOMAIN_SEPARATOR,
+            keccak256(abi.encode(
+                keccak256("TransferWithSignature(address from,address to,uint256 tokenId)"),
+                from,
+                to,
+                tokenId
+            ))
+        ));
+        address signer = ECDSA.recover(hash, signature);
+        require(signer == from, "EventTicket: invalid signature");
+        _transfer(from, to, tokenId);
+    }
+
+       function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC2981)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+
+    function royaltiesReceiver() external view returns(address) {
+        return _royaltiesReceiver;
+    }
+
+
+    function setRoyaltiesReceiver(address newRoyaltiesReceiver)
+    external onlyOwner {
+        require(newRoyaltiesReceiver != _royaltiesReceiver); // dev: Same address
+        _royaltiesReceiver = newRoyaltiesReceiver;
+    }
+
+    function royaltyInfo(uint256 _tokenId, uint256 _salePrice) public view override returns
+  (address receiver, uint256 royaltyAmount) {
+        uint256 _royalties = (_salePrice * royaltiesPercentage) / 100;
+        return (_royaltiesReceiver, _royalties);
+    }
+}
